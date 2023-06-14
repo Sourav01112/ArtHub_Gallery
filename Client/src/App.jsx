5665697f014641b598c78d107f194b41846f635b
Om Prakash Mishra <opsgp1999@gmail.com>
2023-06-14 14:09:28 -0700

    ok

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,11 +3,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./components/Navbar";
+import { AllRoutes } from "./components/Routes/AllRoutes";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <>
       <NavBar />
+   
+    {/* <About/> */}
+    <Contact/>
+     
+
     </>
   );
 }
