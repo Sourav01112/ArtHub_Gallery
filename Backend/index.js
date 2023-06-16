0a927a9c5a7ee045ee8ab268a8901951abc283ad
Sourav01112 <chaudharysourav997@gmail.com>
2023-06-17 00:37:46 +0530

    added admin middleware & Route

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/user.routes");
 const { authMiddleware } = require("./middlewares/auth.middleware");
 const { productRouter } = require("./routes/products.routes");
 const { errorHandler } = require("./middlewares/errorHandle.middleware");
 require("dotenv").config();
+
 const PORT = process.env.PORT;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/user", userRouter);
 // user middleware for user Route
@@ -22,7 +25,7 @@ app.use("/shop", productRouter);
 app.listen(PORT, async () => {
   try {
     await connection;
-    console.log("Server is started on port number", PORT);
+    console.log("Server is running at port number", PORT);
   } catch (error) {
     console.log(error);
   }
--- Backend/models/Products.model.js
+++ Backend/models/Products.model.js
@@ -4,7 +4,7 @@ const productsSchema = mongoose.Schema(
   {
     title: { type: String, required: true },
     desc: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: Array, required: true },
     price: { type: Number, required: true },
     subtitle: { type: String, required: true },
   },
